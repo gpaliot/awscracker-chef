e062b05eba1de44cc5fa5842aba1789f422227be
Guido Paliot <guido@paliot.de>
2015-11-07 22:34:06 +0100

    .

--- cracker/recipes/default.rb
+++ cracker/recipes/default.rb
@@ -3,14 +3,6 @@
 # Recipe:: default
 #
 
-# add the EPEL repo
-yum_repository 'epel' do
-  description 'Extra Packages for Enterprise Linux'
-  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
-  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
-  action :create
-end
-
 # install packages
 package 'bash-completion'
 package 'p7zip'
