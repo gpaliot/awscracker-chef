7fc35209ca41be242cdb6cb32657b7c8246545b8
Guido Paliot <guido@paliot.de>
2015-11-07 23:18:51 +0100

    .

--- cracker/recipes/default.rb
+++ cracker/recipes/default.rb
@@ -17,6 +17,12 @@ remote_file '/root/NVIDIA-Linux-x86_64-352.55.run' do
   action :create
 end
 
+#execute 'nvidia_installer' do
+      #command '/root/NVIDIA-Linux-x86_64-352.55.run -s'
+      #creates ''
+#end
+
+
 # install cudaHashcat
 remote_file '/root/cudaHashcat-1.37.7z' do
   source 'http://hashcat.net/files/cudaHashcat-1.37.7z'
@@ -26,3 +32,8 @@ remote_file '/root/cudaHashcat-1.37.7z' do
   action :create
 end
 
+execute 'hashcat_unpack' do
+    command '/usr/bin/7za x -o/opt /root/cudaHashcat-1.37.7z'
+    command 'ln -sf /opt/cudaHashcat-1.37 /opt/hashcat'
+    creates '/opt/hashcat'
+end
